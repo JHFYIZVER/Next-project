07afa9d241ca8b05f761a9abbdb78b9216fb2d55
Vadim <saraevv205@gmail.com>
2024-09-19 09:53:49 +0900

    add animation menu

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,16 +4,13 @@ import Link from "next/link";
 
 import Basket from "../UI/Basket";
 import "./Header.scss";
-import Input from "../UI/Input";
 import SearchBtn from "../UI/SearchBtn";
 
 const Header = () => {
-  const clazz: string =
-    "header max-w-screen-sm flex justify-between items-center p-5";
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className={isOpen ? clazz + " active" : clazz}>
+    <header className="header max-w-screen-sm w-full flex justify-between items-center p-5">
       <button
         onClick={() => setIsOpen(!isOpen)}
         className={isOpen ? "burger active" : "burger"}
@@ -23,22 +20,23 @@ const Header = () => {
         <i></i>
       </button>
       <nav className={isOpen ? "header-nav active" : "header-nav"}>
-        <ul className="header-nav-list menu flex flex-col gap-8 cursor-pointer">
-          <Link className="menu-item" href="/">
-            Home
-          </Link>
-          <Link className="menu-item" href="/Catalog">
-            Catalog
-          </Link>
-          <Link className="menu-item" href="/Contact">
-            Contact
-          </Link>
-          <Link className="menu-item" href="/Features">
-            Features
-          </Link>
-        </ul>
+        <div>
+          <ul className="header-nav-list flex flex-col gap-8 cursor-pointer">
+            <Link className="nav-list-item" href="/">
+              Home
+            </Link>
+            <Link className="nav-list-item" href="/Catalog">
+              Catalog
+            </Link>
+            <Link className="nav-list-item" href="/Contact">
+              Contact
+            </Link>
+            <Link className="nav-list-item" href="/Features">
+              Features
+            </Link>
+          </ul>
+        </div>
       </nav>
-
       <div className="logo font-bold text-xl">
         <span className="text-blue">Holo</span>Gaze.
       </div>
